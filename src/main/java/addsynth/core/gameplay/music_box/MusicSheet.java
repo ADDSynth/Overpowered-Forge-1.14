00859868410cf7127d206f92877f1727f8efda89
ADDSynth <addsynth@gmail.com>
2020-08-09 18:18:28 -0500

    Fix Problems using the Music Sheet on the Music Box

--- src/main/java/addsynth/core/gameplay/music_box/MusicSheet.java
+++ src/main/java/addsynth/core/gameplay/music_box/MusicSheet.java
@@ -14,6 +14,7 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceContext.FluidMode;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
@@ -21,12 +22,14 @@ import net.minecraft.world.World;
 /**
  * Man did I have trouble with this one. I don't feel like explaining how it works right now.
  * Just run in debug mode and step through the code yourself to see how it works.
- * @see net.minecraft.server.management.PlayerInteractionManager#processRightClickBlock
  * @see net.minecraft.block.TNTBlock#onBlockActivated
  * @see net.minecraft.item.BucketItem
  * @see net.minecraft.item.EnderEyeItem
  */
+@SuppressWarnings("resource")
 public final class MusicSheet extends CoreItem {
+
+  // See how vanilla handles the Ender Eye, an item that is used on blocks and by istelf.
 
   public MusicSheet(final String name){
     super(new Item.Properties().group(ADDSynthCore.creative_tab), name);
@@ -34,42 +37,42 @@ public final class MusicSheet extends CoreItem {
 
   @Override
   public final ActionResult<ItemStack> onItemRightClick(final World world, final PlayerEntity player, final Hand hand){
-    ActionResult<ItemStack> result = null;
     final ItemStack stack = player.getHeldItemMainhand();
-    if(world.isRemote == false){
-      final RayTraceResult raytrace = rayTrace(world, player, FluidMode.NONE);
-      if(raytrace != null){
-        if(raytrace.getType() == RayTraceResult.Type.BLOCK){
-          if(world.getBlockState((new BlockPos(raytrace.getHitVec()))).getBlock() == Core.music_box){
-            result = new ActionResult<ItemStack>(ActionResultType.PASS, stack);
-          }
-        }
-      }
-      if(result == null){
-        if(player.isSneaking()){
-          stack.setTag(null);
-          MessageUtil.send_to_player(player, "gui.addsynthcore.music_sheet.clear");
-          result = new ActionResult<ItemStack>(ActionResultType.SUCCESS, stack);
-        }
+    final RayTraceResult raytrace = rayTrace(world, player, FluidMode.NONE);
+
+    // if we're hitting block
+    if(raytrace.getType() == RayTraceResult.Type.BLOCK){
+      if(world.getBlockState(((BlockRayTraceResult)raytrace).getPos()).getBlock() == Core.music_box){
+        return new ActionResult<ItemStack>(ActionResultType.PASS, stack);
       }
     }
-    return result == null ? new ActionResult<ItemStack>(ActionResultType.PASS, stack) : result;
+    
+    player.setActiveHand(hand);
+    if(world.isRemote == false){
+      if(player.isSneaking()){
+        stack.setTag(null);
+        MessageUtil.send_to_player(player, "gui.addsynthcore.music_sheet.clear");
+        return new ActionResult<ItemStack>(ActionResultType.SUCCESS, stack);
+      }
+    }
+
+    return new ActionResult<ItemStack>(ActionResultType.SUCCESS, stack);
   }
 
   @Override
-  public ActionResultType onItemUse(final ItemUseContext context){
-    final PlayerEntity player = context.getPlayer();
+  public final ActionResultType onItemUse(final ItemUseContext context){
     final World world = context.getWorld();
-    final BlockPos pos = new BlockPos(context.getHitVec());
-    final ItemStack stack = player.getHeldItemMainhand();
+    final BlockPos pos = context.getPos();
     if(world.getBlockState(pos).getBlock() == Core.music_box){
       if(world.isRemote){
-        return ActionResultType.FAIL;
+        return ActionResultType.SUCCESS;
       }
+      final PlayerEntity player = context.getPlayer();
+      final ItemStack stack = context.getItem();
       final TileMusicBox tile = (TileMusicBox)world.getTileEntity(pos);
-      if(tile != null){
+      if(tile != null && player != null){
         if(player.isSneaking() == false){
-          CompoundNBT nbt = stack.getTag();
+          final CompoundNBT nbt = stack.getTag();
           if(nbt != null){
             tile.getMusicGrid().load_from_nbt(nbt);
             tile.update_data();
@@ -77,13 +80,15 @@ public final class MusicSheet extends CoreItem {
             return ActionResultType.SUCCESS;
           }
         }
-        return copy_music_data(stack, player, tile);
+        // is sneaking
+        copy_music_data(stack, player, tile);
+        return ActionResultType.SUCCESS;
       }
     }
     return ActionResultType.PASS;
   }
 
-  private static final ActionResultType copy_music_data(final ItemStack stack, final PlayerEntity player, final TileMusicBox tile){
+  private static final void copy_music_data(final ItemStack stack, final PlayerEntity player, final TileMusicBox tile){
     final CompoundNBT nbt = new CompoundNBT();
     tile.getMusicGrid().save_to_nbt(nbt);
       
@@ -92,13 +97,12 @@ public final class MusicSheet extends CoreItem {
     }
     else{
       stack.shrink(1);
-      final ItemStack music_sheet = new ItemStack(Core.music_sheet,1);
+      final ItemStack music_sheet = new ItemStack(Core.music_sheet, 1);
       music_sheet.setTag(nbt);
       ItemUtil.add_to_player_inventory(player, music_sheet);
     }
       
     MessageUtil.send_to_player(player, "gui.addsynthcore.music_sheet.copy");
-    return ActionResultType.SUCCESS;
   }
 
 }
