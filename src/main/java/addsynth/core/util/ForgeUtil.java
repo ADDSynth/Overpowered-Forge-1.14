f7cfc08e8875464f3541e03164443c20b3c57f83
ADDSynth <addsynth@gmail.com>
2020-09-21 04:25:02 -0500

    Added Minecraft dist checks to ForgeUtil

--- src/main/java/addsynth/core/util/ForgeUtil.java
+++ src/main/java/addsynth/core/util/ForgeUtil.java
@@ -1,7 +1,9 @@
 package addsynth.core.util;
 
 import addsynth.core.ADDSynthCore;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 public final class ForgeUtil {
 
@@ -15,6 +17,18 @@ public final class ForgeUtil {
       ADDSynthCore.log.warn(ForgeUtil.class.getName()+".getModID() returned 'minecraft'. This might not be what you expected. Did you call getModID() at a wrong time?");
     }
     return id;
+  }
+
+  /** Returns true if this is a Client instance of Minecraft.<br />
+   *  This does not check if we're currently executing on the Client side.
+   */
+  public static final boolean isClientInstance(){
+    return FMLEnvironment.dist == Dist.CLIENT;
+  }
+
+  /** Returns true if this is a dedicated server instance. */
+  public static final boolean isDedicatedServer(){
+    return FMLEnvironment.dist == Dist.DEDICATED_SERVER;
   }
 
 }
