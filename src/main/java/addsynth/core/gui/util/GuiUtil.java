e22674bdf383011ad38fb44c211b2bffd6e98e9f
ADDSynth <addsynth@gmail.com>
2022-06-12 12:16:26 -0500

    Deprecate GuiUtil Cached Variables

--- src/main/java/addsynth/core/gui/util/GuiUtil.java
+++ src/main/java/addsynth/core/gui/util/GuiUtil.java
@@ -35,14 +35,19 @@ public final class GuiUtil {
   public final int guiHeight;
 
   /** Left edge of gui. For use in drawing graphics, NOT text! */
+  @Deprecated
   public final int guiLeft;
   /** Right edge of gui. For use in drawing graphics, NOT text! */
+  @Deprecated
   public final int guiRight;
   /** Center of gui. For use in drawing graphics, NOT text! */
+  @Deprecated
   public final int guiCenter;
   /** Top edge of gui. For use in drawing graphics, NOT text! */
+  @Deprecated
   public final int guiTop;
   /** Bottom edge of gui. For use in drawing graphics, NOT text! */
+  @Deprecated
   public final int guiBottom;
 
 
@@ -51,6 +56,7 @@ public final class GuiUtil {
   /** equivalent to xSize - 6. Use for drawing text. */
   public final int right_edge;
 
+  @Deprecated
   public GuiUtil(final ResourceLocation gui_texture, final int gui_width, final int gui_height){
     GUI_TEXTURE = gui_texture;
     guiWidth    = gui_width;
