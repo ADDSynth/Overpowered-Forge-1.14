f750bb3caf2e08fae8769b96880b440c0e699724
ADDSynth <addsynth@gmail.com>
2022-06-14 21:47:30 -0500

    Added Release Candidate to DevStage

--- src/main/java/addsynth/core/util/constants/DevStage.java
+++ src/main/java/addsynth/core/util/constants/DevStage.java
@@ -2,9 +2,15 @@ package addsynth.core.util.constants;
 
 public enum DevStage {
 
-  DEVELOPMENT(true, " (DEVELOPMENT BUILD)"),
+  /** Development of new project. */
   ALPHA(true, "-ALPHA"),
+  /** Done adding new features (feature freeze), now we're fixing bugs. */
   BETA(true, "-BETA"),
+  /** Project is currently being worked on. */
+  DEVELOPMENT(true, " (DEVELOPMENT BUILD)"),
+  /** Development cycle is complete. This is the version we plan to release, unless we find major bugs. */
+  RELEASE_CANDIDATE(false, " Release Candidate"),
+  /** Final version released to the public. */
   STABLE(false, "");
 
   public final boolean isDevelopment;
